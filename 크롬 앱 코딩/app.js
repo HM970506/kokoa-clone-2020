6ecd09c81492feaa4b822d0e72051cf3800ebebc
Hyemi <0506phm@gmail.com>
2021-12-03 12:31:05 +0900

    4주차

---
+++ 크롬 앱 코딩/app.js
@@ -0,0 +1,29 @@
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME="hidden"; //string은 대문자로 표기하는게 관습
+const USERNAME_KEY="username";
+
+function onLoginSubmit(event){
+    event.preventDefault(); //submit 창이동 기능 지움
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username=loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+
+}
+
+function paintGreetings(username){
+    greeting.innerHTML=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+
+if(saveUsername===null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUsername);
+}
